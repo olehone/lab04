b8a3203e6851fcca1f922faf903d36c5ec8e68f6
olehvolodko <oleh.volodko.ri.2022@lpnu.ua>
2022-10-20 02:51:23 +0300

    4.2 lab

---
+++ Source.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+using namespace std;
+int main()
+{
+	double x, xp, xk, dx, A, B, y;
+	cout << "xp = "; cin >> xp;
+	cout << "xk = "; cin >> xk;
+	cout << "dx = "; cin >> dx;
+	cout << fixed;
+	cout << "---------------------------" << endl;
+	cout << "|" << setw(5) << "x" << " |"
+		<< setw(7) << "y" << " |" << endl;
+	cout << "---------------------------" << endl;
+	x = xp;
+	while (x <= xk)
+	{
+		A = 2 / x + fabs(x);
+		if (x < 0)
+			B = 1 + 4 * x * x;
+		else
+			if (x > 2)
+				B = 5 * sin(x * x + 1);
+			else
+				B = exp(x) + fabs(x) * fabs(x);
+		y = A + B;
+		cout << "|" << setw(7) << setprecision(2) << x
+			<< " |" << setw(10) << setprecision(3) << y
+			<< " |" << endl;
+		x += dx;
+	}
+	cout << "---------------------------" << endl;
+	return 0;
+}
